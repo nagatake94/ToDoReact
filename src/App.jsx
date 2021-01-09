69ed4725c41ea7fc599c617ef6583f4ad44d852e
nagatake94 <76759306+nagatake94@users.noreply.github.com>
2021-01-09 14:31:28 +0900

    タスクの追加機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [toDo, setToDo] = useState(["永田"]);
+  const [dones, setDone] = useState(["永田"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodo = [...toDo, todoText];
+    setToDo(newTodo);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="ToDoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="ToDoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">ToDo</p>
         <ul>
-          <div className="list-row">
-            <li>永田</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {toDo.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">Done</p>
         <ul>
-          <div className="list-row">
-            <li>永田</li>
-            <button>戻す</button>
-          </div>
+          {dones.map((done) => {
+            return (
+              <div className="list-row">
+                <li>done</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
